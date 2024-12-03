149bcb1784972dcbd26d7e0ed481de15d5461c40
arman463 <112armanhannani@gmail.com>
2024-12-03 18:28:18 +0330

    Add files via upload

---
+++ balejock.py
@@ -0,0 +1,41 @@
+from bale import *
+import random
+import time
+import requests
+
+
+
+
+bot=Bot(token="2124438657:VjQf6x3U65uXiYjFv9G8xo4KZggNVL4HV1RhfNKR")
+
+
+
+
+@bot.event
+async def on_ready():
+     #await bot.send_message(5580807305,"ربات روشن شد")
+
+     print('\nbot amade ast ')
+
+
+
+
+
+     for i in range(1,50000):
+         try:
+             
+            
+                 j=requests.get("https://one-api.ir/joke/?token=412682:671400d39ff8f")
+                 d=j.json()
+                 d2=d.get('result')
+                 time.sleep(1.5)
+                 await bot.send_message(5192845849,text="جوک شماره "+str(i)+"\n\n"+str(d2)+"\n\n[سازمان جوک 😄😂](https://ble.ir/jokkhanz)")
+                 print("sended "+str(i))
+         except:
+            print("error")
+             
+
+        
+
+bot.run()
+
